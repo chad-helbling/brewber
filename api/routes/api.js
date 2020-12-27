e6cf8c7cd41c0889bf56f69f4df09b988c963189
Chad Helbling <chad.helbling.1@gmail.com>
2020-12-26 19:31:00 -0600

    [CHORE] Removes weather api

--- api/routes/api.js
+++ api/routes/api.js
@@ -15,26 +15,6 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
-router.get('/weather', (req, res) => {
-  var options = {
-    uri: 'http://api.openweathermap.org/data/2.5/weather',
-    qs: {
-        zip:'58554,us',
-        units:'imperial',
-        APPID: '180d91a0913868b97add8df82152bb9a'
-    },
-    json: true // Automatically parses the JSON string in the response
-};
-
-requestP(options)
-    .then((tempData) => res.send(tempData.main))
-    .catch((err) => {
-      console.error(err)
-      res.sendStatus(500);
-    });
-
-})
-
 router.get("/temperature", (req, res) => { 
   try {
     const temperature = arduinoInterface.getTemperature();
